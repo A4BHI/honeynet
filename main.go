0cb72efc224e29144a675a7990de88d47271c0ef
A4BHI <manthukafc@gmail.com>
2026-01-10 18:27:35 +0530

    Testing basic threat engine

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Event struct {
 	IP   string
 	Type string
@@ -9,6 +11,18 @@ type Alert struct {
 	IP       string
 	Severity string
 	Type     string
+}
+
+func ThreatEngineBasic(e Event) {
+	if e.Type == "SSH_LOGIN_FAIL" {
+		alert := Alert{
+			IP:       e.IP,
+			Severity: "HIGH",
+			Type:     e.Type,
+		}
+		fmt.Println("Attack Detected!!")
+		fmt.Println(alert)
+	}
 }
 
 func main() {}
