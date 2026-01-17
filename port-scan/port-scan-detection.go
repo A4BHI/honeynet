01ce124179e1236e31e2f807652fb965a6c1232e
A4BHI <manthukafc@gmail.com>
2026-01-17 20:58:38 +0530

    hmmmm

--- port-scan/port-scan-detection.go
+++ port-scan/port-scan-detection.go
@@ -1,3 +1,17 @@
 package portscan
 
-func 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func AcceptConnRequest() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ln.Accept()
+	ip := ln.Addr().String()
+	fmt.Println(ip)
+}
